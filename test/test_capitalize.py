17884a50dafd73f819a435ea7687326d856e5220
Rachel Ingles <59097320+ringles-veracity@users.noreply.github.com>
2022-04-14 08:23:24 -0400

    Update test_capitalize.py

--- test/test_capitalize.py
+++ test/test_capitalize.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 def capitalize_string(s):
     if not isinstance(s, str):
         raise TypeError('Please provide a string')
     return s.capitalize()
 
 def test_capitalize_string():
-    assert capitalize_string('test') == 'Test'
+    load_dotenv()
+    assert os.environ["XRAY_USERNAME"] == 'Test'
